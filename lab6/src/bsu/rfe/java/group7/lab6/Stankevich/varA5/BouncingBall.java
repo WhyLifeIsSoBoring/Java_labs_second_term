f2c54da2200b46bff29d5d50355735eb7fd2866a
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-03-02 22:02:23 +0300

    feat: add parameters in BouncingBall

--- lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/BouncingBall.java
+++ lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/BouncingBall.java
@@ -1,4 +1,26 @@
 package bsu.rfe.java.group7.lab6.Stankevich.varA5;
 
-public class BouncingBall {
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class BouncingBall implements Runnable {
+
+    private static final int MAX_RADIUS = 40;
+    private static final int MIN_RADIUS = 3;
+    private static final int MAX_SPEED = 15;
+
+    private Field field;
+    private int radius;
+    private Color color;
+
+    private double x;
+    private double y;
+
+    private int speed;
+    private double speedX;
+    private double speedY;
+
+    public void run() {
+        
+    }
 }
--- lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/Field.java
+++ lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/Field.java
@@ -1,4 +1,11 @@
 package bsu.rfe.java.group7.lab6.Stankevich.varA5;
 
-public class Field {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Field extends JPanel {
+
 }
