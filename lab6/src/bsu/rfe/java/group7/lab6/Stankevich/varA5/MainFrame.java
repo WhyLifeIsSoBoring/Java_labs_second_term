402d635b7b75f58c15770d0d3871d97be37e46e4
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-03-02 21:33:22 +0300

    feat: add create MainFrame

---
+++ lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/MainFrame.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group7.lab6.Stankevich.varA5;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
