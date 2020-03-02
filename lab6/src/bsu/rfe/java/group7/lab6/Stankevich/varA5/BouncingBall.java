9cdda771b124f0df0355fbba1860749d6901db62
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-03-02 22:06:21 +0300

    feat: add constructor of BouncingBall class

--- lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/BouncingBall.java
+++ lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/BouncingBall.java
@@ -20,7 +20,30 @@ public class BouncingBall implements Runnable {
     private double speedX;
     private double speedY;
 
+    public BouncingBall(Field field) {
+
+        this.field = field;
+        radius = new Double(Math.random() * (MAX_RADIUS - MIN_RADIUS)).intValue() + MIN_RADIUS;
+
+        speed = new Double(Math.round(5 * MAX_SPEED / radius)).intValue();
+        if (speed > MAX_SPEED) {
+            speed = MAX_SPEED;
+        }
+
+        double angle = Math.random() * 2 * Math.PI;
+
+        speedX = 3 * Math.cos(angle);
+        speedY = 3 * Math.sin(angle);
+
+        color = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+
+        x = Math.random() * (field.getSize().getWidth() - 2 * radius) + radius;
+        y = Math.random() * (field.getSize().getHeight() - 2 * radius) + radius;
+
+        Thread thisThread = new Thread(this);
+        thisThread.start();
+    }
     public void run() {
-        
+
     }
 }
