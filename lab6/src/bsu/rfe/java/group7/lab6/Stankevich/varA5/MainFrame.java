5d0025db6eb850d611a6ccab25d839e8ec5d5102
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-03-02 23:28:53 +0300

    feat: add menubar with 2 tabs: balls and control

--- lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/MainFrame.java
+++ lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/MainFrame.java
@@ -2,11 +2,17 @@ package bsu.rfe.java.group7.lab6.Stankevich.varA5;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame {
 
     private static final int WIDTH = 700;
     private static final int HEIGHT = 500;
+
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+
+    private Field field = new Field();
 
     public MainFrame() {
         super("Программирование и синхронизация потоков");
@@ -15,6 +21,41 @@ public class MainFrame extends JFrame {
         setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGHT) / 2);
         setExtendedState(MAXIMIZED_BOTH);
 
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+                    pauseMenuItem.setEnabled(true);
+                }
+            }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+        getContentPane().add(field, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
