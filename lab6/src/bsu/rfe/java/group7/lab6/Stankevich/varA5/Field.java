84d7c53bef6df1d2d8648c67f00a9ce4046712f7
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-03-02 23:19:11 +0300

    feat: add some methods to work with streams

--- lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/Field.java
+++ lab6/src/bsu/rfe/java/group7/lab6/Stankevich/varA5/Field.java
@@ -29,4 +29,24 @@ public class Field extends JPanel {
             ball.paint(canvas);
         }
     }
+
+    public void addBall() {
+        balls.add(new BouncingBall(this));
+    }
+
+    public synchronized void pause() {
+        paused = true;
+    }
+
+    public synchronized void resume() {
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+            wait();
+        }
+    }
 }
