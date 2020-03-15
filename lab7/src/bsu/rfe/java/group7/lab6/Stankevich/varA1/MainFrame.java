5e43eb777fdd5b8a858138584d848fbd16ff6e17
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-03-15 11:31:58 +0300

    feat: add base fields,button and components of MainFrame

--- lab7/src/bsu/rfe/java/group7/lab6/Stankevich/varA1/MainFrame.java
+++ lab7/src/bsu/rfe/java/group7/lab6/Stankevich/varA1/MainFrame.java
@@ -2,14 +2,27 @@ package bsu.rfe.java.group7.lab6.Stankevich.varA1;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-import javax.swing.SwingUtilities;
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
     private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
     private static final int FRAME_MINIMUM_WIDTH = 500;
     private static final int FRAME_MINIMUM_HEIGHT = 500;
+
+    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+
+
+    private final JTextField textFieldFrom;
+    private final JTextField textFieldTo;
+    private final JTextArea textAreaIncoming;
+    private final JTextArea textAreaOutgoing;
 
 
     public MainFrame() {
@@ -19,6 +32,34 @@ public class MainFrame extends JFrame {
         final Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - getWidth()) / 2, (kit.getScreenSize().height - getHeight()) / 2);
 
+        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+
+        final JScrollPane scrollPaneIncoming = new JScrollPane(textAreaIncoming);
+
+        final JLabel labelFrom = new JLabel("Подпись");
+        final JLabel labelTo = new JLabel("Получатель");
+
+        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+
+        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+
+        final JScrollPane scrollPaneOutgoing = new JScrollPane(textAreaOutgoing);
+
+        final JPanel messagePanel = new JPanel();
+        messagePanel.setBorder(BorderFactory.createTitledBorder("Сообщение"));
+
+        final JButton sendButton = new JButton("Отправить");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
     }
 
     public static void main(String[] args) {
