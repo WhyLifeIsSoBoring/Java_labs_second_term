cb87385d525c56b25aa5cbf5116c88ede2653c17
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-04-20 15:30:04 +0300

    feat: add NewMessageServlet

---
+++ lab/src/bsu/rfe/java/group7/lab8/Stankevich/varA1/servlet/NewMessageServlet.java
@@ -0,0 +1,30 @@
+package bsu.rfe.java.group7.lab8.Stankevich.varA1.servlet;
+
+import java.io.IOException;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bsu.rfe.java.group7.lab8.Stankevich.varA1.entity.ChatMessage;
+import bsu.rfe.java.group7.lab8.Stankevich.varA1.entity.ChatUser;
+
+public class NewMessageServlet extends ChatServlet {
+
+    private static final long serialVersionUID = 1L;
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        String message = (String)request.getParameter("message");
+
+        if (message!=null && !"".equals(message)) {
+            ChatUser author = activeUsers.get((String)
+                    request.getSession().getAttribute("name"));
+            synchronized (messages) {
+                messages.add(new ChatMessage(message, author, Calendar.getInstance().getTimeInMillis()));
+            }
+        }
+        response.sendRedirect("/chat/compose_message.htm");
+    }
+}
